9b3aa3670a83f7259688e506a1f9279d3bfb58cb
Christian Lascsak <christian.lascsak@anecon.com>
2021-06-17 22:16:40 +0200

    port to windows

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "StreamerClient/StreamerClient.h"
-
-int main(int argc, char const *argv[])
-{
-    StreamerClient client = StreamerClient();
-    client.receiveAndEncode();
-    return 0;
-}
